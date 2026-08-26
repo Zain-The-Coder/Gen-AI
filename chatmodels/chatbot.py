6c3c2d4ca76e480f630f14504ae641aa8badc3d6
Zain-The-Coder <zain015976@gmail.com>
2026-08-27 01:25:25 +0500

    mainntaing llms history

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -2,10 +2,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_mistralai import ChatMistralAI
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage, AIMessage , SystemMessage
 
 model = ChatMistralAI(model="mistral-small-2506", temperature=0.1, max_tokens=200)
-messages = []
+messages = [
+    SystemMessage(content = "You are a funny AI agent")
+]
 
 print("________Type 0 to exit the application_________")
 while True:
